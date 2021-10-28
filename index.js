4fd067f0f01d0522d8ed27e7c5f9f0fe6b8607fc
Sebastian <sebas6292@gmail.com>
2021-10-28 14:35:14 -0700

    wip2

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('web47 rocks!')
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send(`
+    <h1>Web 47 rocks!</h1>
+    `)
+})
+
+app.get('/port', (req, res) => {
+    res.send(`
+    <div> port is ${process.env.PORT}</div>
+    `)
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+  })
+
+
+function cohortRocks() { //eslint-disable-line
+    console.log('arguments:', process.argv.slice(2))
+    console.log(`web ${process.argv[2]} rocks`) // fix it using process.argv !
+  }
+  function cohortRocksEnv() {
+    console.log(`Home folder is "${process.env.HOME}"`)
+    console.log(`My shell is "${process.env.SHELL}"`)
+    console.log(`In my environment FOO is "${process.env.FOO}"`)
+    console.log(`My super secret comes from "${process.env.SUPER_SECRET}"`)
+  }
+  cohortRocksEnv()
